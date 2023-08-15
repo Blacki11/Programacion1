95b2015be08a46c08692e49faf0897978708bb38
matijofre <mati.jofre3@gmail.com>
2023-08-15 10:31:09 -0300

    primer

---
+++ Ejcomp.py
@@ -0,0 +1,6 @@
+#Crear una variable llamada numero1 y elegir un numero entero
+numero1 = 6
+#Crear otra variable llamada numero2 y elegir numero decimal
+numero2 = 10.5
+#Realizar suma de las variables
+suma = (numero1 + numero2)
