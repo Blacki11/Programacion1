67b51899223e3375312351c864fea1b3edb56622
matijofre <mati.jofre3@gmail.com>
2023-08-16 19:31:02 -0300

    Seg

--- Ejcomp.py
+++ Ejcomp.py
@@ -4,3 +4,53 @@ numero1 = 6
 numero2 = 10.5
 #Realizar suma de las variables
 suma = (numero1 + numero2)
+print(suma)
+#Variables resta, multiplicacion y division
+resta = (numero1 - numero2)
+print(resta)
+mult = (numero1 * numero2)
+print(mult)
+divis = (numero1 / numero2)
+print(divis)
+#Crear una variable nombre y asignarle un valor
+nombre = "Matias"
+print(nombre)
+#Crear las variables precio y descuento y asignarles valores
+precio = 350
+descuento = 0.35
+#Calcular el precio final
+precio_final = (precio *(1 - descuento))
+print(precio_final)
+#Crear una variable llamada cadena y asignarle un texto
+cadena = "Hoy hace frio"
+#crear una variable llamada longitud.
+longitud = len(cadena)
+#Variable precio x2, pasar de valor decimal a entero
+precio = 5.50
+precio_entero = int(precio)
+print(precio_entero)
+#Variables nombre y apellido y concatenarlas
+nombre = "Matias"
+apellido = "Jofre"
+nombre_completo = (nombre + " " + apellido)
+print(nombre_completo)
+#Variable edad, incrementa y disminuye
+edad = 24
+print(edad)
+edad += 5
+print(edad)
+edad -=10
+print(edad)
+#Variable altura, se multiplica y divide
+altura = 1.83
+print(altura)
+altura *= 4
+print(altura)
+altura /= 3
+print(altura)
+#Variable mayus, pasa a minusculas y 
+nombre = "MATIAS JOFRE"
+nom_minus = nombre.lower()
+print(nom_minus)
+nomb = nombre.title()
+print(nomb)
