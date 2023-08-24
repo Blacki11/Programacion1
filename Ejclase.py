00ca2dddc8ad5d8d43072b42ef7527275a289c25
matijofre <mati.jofre3@gmail.com>
2023-08-24 09:19:00 -0300

    tp2

--- Ejclase.py
+++ Ejclase.py
@@ -12,7 +12,7 @@ elif(num2)<1 or num2>12:
     print("mes erroneo")
 else:
     print(dia, ",", num1, "/", num2)
-    if dia in ["lunes", "martes", "miercoles"]: #Se solicita informacion para saber si en estos dias se tomaron examenes.
+    if dia in ["lunes", "martes", "miercoles"]:     #Se solicita informacion para saber si en estos dias se tomaron examenes.
             examenes1=(input("Se tomaron examenes?(Si/No): ")).lower()
             if (examenes1=="si"):
                 aprob=int(input("Ingrese la cantidad de alumnos aprobados: "))
@@ -24,13 +24,13 @@ else:
                 print("No se tomaron examenes ese dia para ese nivel")
             else:
                 print("Respuesta Invalida")
-    elif dia in ["jueves"]: #Se solicita el % de alumnos que asisten a clase
+    elif dia in ["jueves"]:              #Se solicita el % de alumnos que asisten a clase
             porc_asist=float(input("Ingrese el porcentaje de alumnos que asistieron a clase: "))
             if porc_asist>50:
                 print("Asistio la mayoria")
             else:
                 print("No asistio la mayoria")
-    elif dia in ["viernes"] and (num2==1 or num2==7) and num1 == 1: #Comienzo de ciclo de "Ingles para viajeros"
+    elif dia in ["viernes"] and (num2==1 or num2==7) and num1 == 1:               #Comienzo de ciclo de "Ingles para viajeros"
             cant_alumn=int(input("Ingrese la cantidad de alumnos del nuevo ciclo: "))
             aranc_alumn=float(input("Ingrese el arancel por alumno: "))
             ingTot=cant_alumn*aranc_alumn
